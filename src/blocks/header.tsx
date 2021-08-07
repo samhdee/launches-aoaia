98da3c92e5fc3c1dbb4cc7d71ec595fbba594589
Clara Dhaleine <clara_dhaleine@fastmail.com>
2021-08-07 22:32:21 +0200

    Rearranging project, cleaning up

---
+++ src/blocks/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "../assets/scss/header.scss";
+
+export default function Header() {
+    return (
+        <header className="app-header">
+            <a href="/">Modifier votre date de naissance</a>
+        </header>
+    );
+}
